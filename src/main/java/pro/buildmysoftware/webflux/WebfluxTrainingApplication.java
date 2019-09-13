e3d1922a60ab770cbb6f5a3b105fc4f72638835a
Mike Wojtyna <mike@buildmysoftware.pro>
2019-09-13 20:38:51 +0200

    Init project

---
+++ src/main/java/pro/buildmysoftware/webflux/WebfluxTrainingApplication.java
@@ -0,0 +1,13 @@
+package pro.buildmysoftware.webflux;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebfluxTrainingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebfluxTrainingApplication.class, args);
+	}
+
+}
