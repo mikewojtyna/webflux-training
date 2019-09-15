08686bce7a297a764fd72a0784e3387e39810d44
Mike Wojtyna <mike@buildmysoftware.pro>
2019-09-15 21:11:03 +0200

    Add map and flatMap examples

---
+++ src/test/java/pro/buildmysoftware/webflux/examples/OperatorExamplesTest.java
@@ -0,0 +1,28 @@
+package pro.buildmysoftware.webflux.examples;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import pro.buildmysoftware.webflux.hello.HelloHandler;
+import reactor.core.publisher.Mono;
+import reactor.test.StepVerifier;
+
+public class OperatorExamplesTest {
+	@DisplayName("map example")
+	@Test
+	void mapExample() throws Exception {
+		StepVerifier.create(Mono.just("hello").log()
+			.map(HelloHandler.Hello::new).map(m -> m))
+			.expectNext(new HelloHandler.Hello("hello"))
+			.expectComplete().verify();
+	}
+
+	@DisplayName("flat map example")
+	@Test
+	void flatMapExample() throws Exception {
+		StepVerifier.create(Mono.just("hello").log()
+			.map(HelloHandler.Hello::new)
+			.flatMap(m -> Mono.fromCallable(() -> m)))
+			.expectNext(new HelloHandler.Hello("hello"))
+			.verifyComplete();
+	}
+}
