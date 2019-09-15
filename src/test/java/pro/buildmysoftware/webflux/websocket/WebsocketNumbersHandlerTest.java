52f7c98ecc78d384eaa3f41f25e3f2b785186277
Mike Wojtyna <mike@buildmysoftware.pro>
2019-09-15 23:10:21 +0200

    Configure websocket test to use random port web environment

--- src/test/java/pro/buildmysoftware/webflux/websocket/WebsocketNumbersHandlerTest.java
+++ src/test/java/pro/buildmysoftware/webflux/websocket/WebsocketNumbersHandlerTest.java
@@ -3,19 +3,24 @@ package pro.buildmysoftware.webflux.websocket;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.web.server.LocalServerPort;
 import org.springframework.web.reactive.socket.WebSocketMessage;
 import org.springframework.web.reactive.socket.client.ReactorNettyWebSocketClient;
 import org.springframework.web.reactive.socket.client.WebSocketClient;
 
 import java.net.URI;
+import java.net.URISyntaxException;
 import java.time.Duration;
 import java.util.ArrayList;
 import java.util.Collection;
 
 import static org.assertj.core.api.Assertions.assertThat;
 
-@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.DEFINED_PORT)
+@SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
 public class WebsocketNumbersHandlerTest {
+	@LocalServerPort
+	private String port;
+
 	// @formatter:off
 	@DisplayName(
 		"when execute on /websocket/hello, " +
@@ -29,13 +34,17 @@ public class WebsocketNumbersHandlerTest {
 		WebSocketClient client = new ReactorNettyWebSocketClient();
 
 		// when
-		client.execute(new URI("ws://localhost:8080/websocket/hello"),
-			session -> session
+		client.execute(uri("/websocket/hello"), session -> session
 			.receive().map(WebSocketMessage::getPayloadAsText)
 			.doOnNext(messages::add).then())
 			.block(Duration.ofSeconds(5));
 
 		// then
 		assertThat(messages).containsExactlyInAnyOrder("1", "2", "3");
+	}
+
+	private URI uri(String path) throws URISyntaxException {
+		return new URI(String
+			.format("ws://localhost:%s/websocket/hello", port));
 	}
 }
