4a6cf1519f0ff76d8cd02ebc5ca42ed3c6683118
Mike Wojtyna <mike@buildmysoftware.pro>
2019-09-18 00:27:34 +0200

    Update direct processor example to use sink directly

--- src/test/java/pro/buildmysoftware/webflux/examples/processors/DirectorProcessorExampleTest.java
+++ src/test/java/pro/buildmysoftware/webflux/examples/processors/DirectorProcessorExampleTest.java
@@ -4,27 +4,27 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import reactor.core.publisher.DirectProcessor;
 import reactor.core.publisher.Flux;
+import reactor.core.publisher.FluxSink;
 
 public class DirectorProcessorExampleTest {
 	@DisplayName("direct processor example (documentation example)")
 	@Test
 	void test() throws Exception {
 		DirectProcessor<String> hotSource = DirectProcessor.create();
-
 		Flux<String> hotFlux = hotSource.map(String::toUpperCase);
-
+		FluxSink<String> sink = hotSource.sink();
 
 		hotFlux.subscribe(d -> System.out
 			.println("Subscriber 1 to Hot Source: " + d));
 
-		hotSource.onNext("blue");
-		hotSource.onNext("green");
+		sink.next("blue");
+		sink.next("green");
 
 		hotFlux.subscribe(d -> System.out
 			.println("Subscriber 2 to Hot Source: " + d));
 
-		hotSource.onNext("orange");
-		hotSource.onNext("purple");
-		hotSource.onComplete();
+		sink.next("orange");
+		sink.next("purple");
+		sink.complete();
 	}
 }
