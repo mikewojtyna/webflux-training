f656da12d3836e1273686c2475bff86f3fc81ee6
Mike Wojtyna <mike@buildmysoftware.pro>
2019-09-17 10:38:14 +0200

    Add finite and infinite flux examples

---
+++ src/test/java/pro/buildmysoftware/webflux/operators/FiniteAndInfiniteFluxExamplesTest.java
@@ -0,0 +1,38 @@
+package pro.buildmysoftware.webflux.operators;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import reactor.core.publisher.Flux;
+
+import java.time.Duration;
+
+public class FiniteAndInfiniteFluxExamplesTest {
+	@DisplayName("show map operator")
+	@Test
+	void mapFinite() throws Exception {
+		Flux<String> flux = Flux.just("hi", "hello");
+
+		flux.log().map(String::length).filter(l -> l > 2)
+			.subscribe(System.out::println);
+	}
+
+	@DisplayName("show working on infinite stream")
+	@Test
+	void infinite() throws Exception {
+		Flux<Long> flux = Flux.interval(Duration.ofSeconds(1));
+
+		flux.log().subscribe(l -> {
+			try {
+				Thread.sleep(5000);
+				System.out.println("Subscriber 0 " + l);
+			}
+			catch (InterruptedException e) {
+				e.printStackTrace();
+			}
+		});
+		flux.log().subscribe(l -> System.out
+			.println("Subscriber 1: " + l));
+
+		Thread.sleep(2000);
+	}
+}
