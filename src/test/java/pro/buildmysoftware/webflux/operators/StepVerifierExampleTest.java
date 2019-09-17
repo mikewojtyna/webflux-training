62dac9181d8b4f408883c9e841e378a86450d66f
Mike Wojtyna <mike@buildmysoftware.pro>
2019-09-17 10:47:20 +0200

    Add step verifier example

---
+++ src/test/java/pro/buildmysoftware/webflux/operators/StepVerifierExampleTest.java
@@ -0,0 +1,33 @@
+package pro.buildmysoftware.webflux.operators;
+
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.reactivestreams.Publisher;
+import reactor.core.publisher.Flux;
+import reactor.test.StepVerifier;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class StepVerifierExampleTest {
+	// @formatter:off
+	@DisplayName(
+		"show how to use step verifier"
+	)
+	// @formatter:on
+	@Test
+	void test() throws Exception {
+		Publisher<String> flux = Flux.just(1, 2, 3)
+			.map(String::valueOf);
+
+		StepVerifier.create(flux)
+			// @formatter:off
+
+			.expectSubscription()
+			.expectNext("1")
+			.assertNext(s -> assertThat(s).isEqualTo(
+				"2"))
+			.expectNextMatches(s -> "3".equals(s))
+// @formatter:on
+			.verifyComplete();
+	}
+}
