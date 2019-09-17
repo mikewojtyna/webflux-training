8247aee9898a0db15bcb37f99c8a482a9101e7b4
Mike Wojtyna <mike@buildmysoftware.pro>
2019-09-17 15:14:56 +0200

    Change web socket number test to be properly named integration

--- src/test/java/pro/buildmysoftware/webflux/web/websocket/WebsocketNumbersHandlerIntegrationTest.java
+++ src/test/java/pro/buildmysoftware/webflux/web/websocket/WebsocketNumbersHandlerIntegrationTest.java
@@ -17,7 +17,7 @@ import java.util.Collection;
 import static org.assertj.core.api.Assertions.assertThat;
 
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
-public class WebsocketNumbersHandlerTest {
+public class WebsocketNumbersHandlerIntegrationTest {
 	@LocalServerPort
 	private String port;
 
