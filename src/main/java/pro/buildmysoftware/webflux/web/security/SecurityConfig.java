81f357b09555091f5cc1248600b4d7909b2e6e49
Mike Wojtyna <mike@buildmysoftware.pro>
2019-09-18 06:48:05 +0200

    Add basic security

---
+++ src/main/java/pro/buildmysoftware/webflux/web/security/SecurityConfig.java
@@ -0,0 +1,27 @@
+package pro.buildmysoftware.webflux.web.security;
+
+import org.springframework.context.annotation.Bean;
+import org.springframework.security.config.annotation.web.reactive.EnableWebFluxSecurity;
+import org.springframework.security.config.web.server.ServerHttpSecurity;
+import org.springframework.security.core.userdetails.MapReactiveUserDetailsService;
+import org.springframework.security.core.userdetails.User;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.web.server.SecurityWebFilterChain;
+
+@EnableWebFluxSecurity
+public class SecurityConfig {
+	@Bean
+	public MapReactiveUserDetailsService userDetailsService() {
+		UserDetails user = User.withUsername("user")
+			.password("{noo}user").roles("USER").build();
+		return new MapReactiveUserDetailsService(user);
+	}
+
+	@Bean
+	public SecurityWebFilterChain springSecurityFilterChain(ServerHttpSecurity http) {
+		http.authorizeExchange().pathMatchers("/secured")
+			.authenticated().anyExchange().permitAll().and()
+			.httpBasic();
+		return http.build();
+	}
+}
