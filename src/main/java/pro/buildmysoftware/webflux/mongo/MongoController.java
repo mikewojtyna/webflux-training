6da44a28d28135913c1bfc830b37c2cc2fb4f290
Mike Wojtyna <mike@buildmysoftware.pro>
2019-09-18 13:27:17 +0200

    Remove unused dependency in mongo controller

--- src/main/java/pro/buildmysoftware/webflux/mongo/MongoController.java
+++ src/main/java/pro/buildmysoftware/webflux/mongo/MongoController.java
@@ -1,6 +1,5 @@
 package pro.buildmysoftware.webflux.mongo;
 
-import org.springframework.data.mongodb.core.ReactiveMongoTemplate;
 import org.springframework.http.MediaType;
 import org.springframework.web.bind.annotation.*;
 import reactor.core.publisher.Flux;
@@ -10,12 +9,9 @@ import reactor.core.publisher.Mono;
 @RequestMapping("/controller/mongo")
 public class MongoController {
 	private MessageRepository messageRepository;
-	private ReactiveMongoTemplate template;
 
-	public MongoController(MessageRepository messageRepository,
-			       ReactiveMongoTemplate template) {
+	public MongoController(MessageRepository messageRepository) {
 		this.messageRepository = messageRepository;
-		this.template = template;
 	}
 
 	@GetMapping(produces = MediaType.TEXT_EVENT_STREAM_VALUE)
