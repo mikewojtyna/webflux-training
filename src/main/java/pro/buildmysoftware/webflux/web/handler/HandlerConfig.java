e228c0c9869e5ef8b260b9e9e6898d69d6b303e6
Mike Wojtyna <mike@buildmysoftware.pro>
2019-11-27 13:09:08 +0100

    Use static imports in hello handler

--- src/main/java/pro/buildmysoftware/webflux/web/handler/HandlerConfig.java
+++ src/main/java/pro/buildmysoftware/webflux/web/handler/HandlerConfig.java
@@ -2,16 +2,18 @@ package pro.buildmysoftware.webflux.web.handler;
 
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
-import org.springframework.web.reactive.function.server.RequestPredicates;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
 
+import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
+
 @Configuration
 public class HandlerConfig {
+
 	@Bean
 	public RouterFunction<ServerResponse> route(HelloHandler helloHandler) {
-		return RouterFunctions.route(RequestPredicates
-			.GET("/hello"), request -> helloHandler.hello());
+		return RouterFunctions
+			.route(GET("/hello"), helloHandler::hello);
 	}
 }
--- src/main/java/pro/buildmysoftware/webflux/web/handler/HelloHandler.java
+++ src/main/java/pro/buildmysoftware/webflux/web/handler/HelloHandler.java
@@ -5,13 +5,15 @@ import org.springframework.http.server.reactive.ServerHttpResponse;
 import org.springframework.stereotype.Component;
 import org.springframework.web.reactive.function.BodyInserter;
 import org.springframework.web.reactive.function.BodyInserters;
+import org.springframework.web.reactive.function.server.ServerRequest;
 import org.springframework.web.reactive.function.server.ServerResponse;
 import pro.buildmysoftware.webflux.web.Hello;
 import reactor.core.publisher.Mono;
 
 @Component
 public class HelloHandler {
-	public Mono<ServerResponse> hello() {
+
+	public Mono<ServerResponse> hello(ServerRequest request) {
 		return ServerResponse.ok()
 			.contentType(MediaType.APPLICATION_JSON_UTF8)
 			.body(bodyInserter());
