21168377c5423954f03788de9290bff7d8fb2ee5
Mike Wojtyna <mike@buildmysoftware.pro>
2019-12-13 11:21:23 +0100

    Add handler function config using lambda

--- src/main/java/pro/buildmysoftware/webflux/web/handler/HandlerConfig.java
+++ src/main/java/pro/buildmysoftware/webflux/web/handler/HandlerConfig.java
@@ -5,6 +5,8 @@ import org.springframework.context.annotation.Configuration;
 import org.springframework.web.reactive.function.server.RouterFunction;
 import org.springframework.web.reactive.function.server.RouterFunctions;
 import org.springframework.web.reactive.function.server.ServerResponse;
+import pro.buildmysoftware.webflux.web.Hello;
+import reactor.core.publisher.Mono;
 
 import static org.springframework.web.reactive.function.server.RequestPredicates.GET;
 
@@ -14,10 +16,16 @@ public class HandlerConfig {
 	@Bean
 	public RouterFunction<ServerResponse> route(HelloHandler helloHandler,
 						    MessagesHandler messagesHandler) {
+		// @formatter:off
 		return RouterFunctions.route(GET("/hello"),
 			helloHandler::hello)
-			.and(RouterFunctions
-				.route(GET("/messages"),
-					messagesHandler::handle));
+			.and(
+				RouterFunctions.route(GET("/messages"),
+					messagesHandler::handle))
+			.and(RouterFunctions.route(
+				GET("/messages2"),
+				serverRequest -> ServerResponse.ok().body(Mono.just(new Hello("hi")), Hello.class)))
+		// @formatter:on
+			;
 	}
 }
